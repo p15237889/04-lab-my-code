ba158a2494ef7a9fff68591658a5b236e81e5ef4
Mark Rankin <P15237889@LEC-ADMIN.dmu.ac.uk>
2018-10-31 20:31:16 +0000

    04-lab complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log("hello");
+const menuToggle = document.getElementById("menu-toggle");
+const menuNav  = document.getElementById("menu-nav");
+
+
+menuToggle.addEventListener("click", () => {
+    console.log("called toggleMenu");
+    menuNav.classList.toggle("menu-toggle");
+ });
